f9317889f108c53a88b75b5406134871fe327c66
Hugo Otero <hugootero94@gmail.com>
2023-10-27 20:18:11 +0200

    array de objetos inicial con los electrodomesticos a usar

---
+++ js/electrodomesticos.js
@@ -0,0 +1,46 @@
+datosElectrodomesticos = [
+  {
+    electrodomestico: "estufa",
+    consumo: 1000,
+  },
+  {
+    electrodomestico: "nevera",
+    consumo: 1000,
+  },
+  {
+    electrodomestico: "congelador",
+    consumo: 1000,
+  },
+  {
+    electrodomestico: "vitroceramica",
+    consumo: 1000,
+  },
+  {
+    electrodomestico: "pc",
+    consumo: 1000,
+  },
+  {
+    electrodomestico: "tv",
+    consumo: 1000,
+  },
+  {
+    electrodomestico: "lavadora",
+    consumo: 1000,
+  },
+  {
+    electrodomestico: "microondas",
+    consumo: 1000,
+  },
+  {
+    electrodomestico: "lavavajillas",
+    consumo: 1000,
+  },
+  {
+    electrodomestico: "horno",
+    consumo: 1000,
+  },
+  {
+    electrodomestico: "estufa",
+    consumo: 1000,
+  },
+];
