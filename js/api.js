a8a6d016ff8b8deba7e616f8d1c1e0d33101a04a
githubpersona <victo@victoriano.none>
2023-10-29 10:40:01 +0100

    Integrada caché de datos de 5 min en api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,7 @@
 "use strict";
 import { datosElectrodomesticos } from "./electrodomesticos.js";
 // console.log(datosElectrodomesticos);
+const precioEnWatiosHora = [];
 
 async function obtenerDatos() {
   try {
@@ -8,7 +9,7 @@ async function obtenerDatos() {
       "https://bypass-cors-beta.vercel.app/?url=https://api.preciodelaluz.org/v1/prices/all?zone=PCB"
     );
     const procesado = await res.json();
-    console.log(procesado);
+    // console.log(procesado);
 
     const resultados = [];
 
@@ -18,7 +19,7 @@ async function obtenerDatos() {
       const unidades = procesado.data[clave].units;
       resultados.push({ hora, precio, unidades });
     }
-    console.log(resultados);
+    // console.log(resultados);
 
     return resultados;
   } catch (error) {
@@ -27,53 +28,106 @@ async function obtenerDatos() {
   }
 }
 
-obtenerDatos().then((resultados) => {
-  if (resultados) {
-    // console.log(resultados);
-    let fechaActual = new Date();
-    let horaActual = fechaActual.getHours();
-    console.log(horaActual);
+function obtenerDatos2() {
+  return obtenerDatos().then((resultados) => {
+    if (resultados) {
+      let costeElectrodomestico = {};
+      // console.log(resultados);
+      let fechaActual = new Date();
+      let horaActual = fechaActual.getHours();
+      // console.log(horaActual);
 
-    function redondearDecimal(numero, decimales) {
-      let factor = Math.pow(10, decimales);
-      return Math.round(numero * factor) / factor;
-    }
-
-    for (let datos of resultados) {
-      const horaToString = parseFloat(datos.hora);
-      // console.log(horaToString);
-      if (horaActual === horaToString) {
-        console.log(datos.precio);
-        const precioEnWatiosHora = [];
-        for (let objeto of datosElectrodomesticos) {
-          const electrodomestico = objeto.electrodomestico;
-          const consumo = (datos.precio * objeto.consumo) / 1000000;
-          precioEnWatiosHora.push({
-            electrodomestico: electrodomestico,
-            consumo: redondearDecimal(consumo, 4),
-          });
-        }
-        const pAct = document.querySelectorAll(".precioAct");
-        console.log(pAct);
-        pAct.forEach((p, index) => {
-          p.textContent = `${precioEnWatiosHora[index].consumo} €/wh`;
-        });
-        console.log(precioEnWatiosHora);
+      function redondearDecimal(numero, decimales) {
+        let factor = Math.pow(10, decimales);
+        return Math.round(numero * factor) / factor;
       }
-    }
 
-    const precioMax = resultados.sort((a, b) => {
-      return b.precio - a.precio;
-    });
-    const precioMaximo = precioMax[0];
-    const precioMinimo = precioMax[precioMax.length - 1];
-    console.log(precioMaximo);
-    console.log(precioMinimo);
-    console.log(precioMax);
-    console.log(
-      `la hora mas cara es: ${precioMaximo.hora}, la hora mas barata es ${precioMinimo.hora}`
-    );
-  } else {
-    console.log("Hubo un error al obtener los datos.");
+      for (let datos of resultados) {
+        const horaToString = parseFloat(datos.hora);
+        // console.log(horaToString);
+        if (horaActual === horaToString) {
+          // console.log(datos.precio);
+          for (let objeto of datosElectrodomesticos) {
+            const electrodomestico = objeto.electrodomestico;
+            const coste = (datos.precio * objeto.consumo) / 1000000;
+            precioEnWatiosHora.push({
+              electrodomestico: electrodomestico,
+              coste: redondearDecimal(coste, 4),
+            });
+          }
+
+          const pAct = document.querySelectorAll(".precioAct");
+          // console.log(pAct);
+          pAct.forEach((p, index) => {
+            p.textContent = `${precioEnWatiosHora[index].coste} €/wh`;
+          });
+          // console.log(precioEnWatiosHora);
+        }
+      }
+
+      const precioMax = resultados.sort((a, b) => {
+        return b.precio - a.precio;
+      });
+      const precioMaximo = precioMax[0];
+      const precioMinimo = precioMax[precioMax.length - 1];
+      // console.log(precioMaximo);
+      // console.log(precioMinimo);
+      // console.log(precioMax);
+      console.log(
+        `la hora mas cara es: ${precioMaximo.hora}, la hora mas barata es ${precioMinimo.hora}`
+      );
+    } else {
+      console.log("Hubo un error al obtener los datos.");
+    }
+    return precioEnWatiosHora;
+  });
+}
+
+// cache datos
+
+const ahora = new Date();
+// let resultados = [];
+
+async function obtenerResultados() {
+  let resultados = [];
+
+  resultados = obtenerResultadosCache();
+  // console.log(resultados);
+  // si resultados es falso, hay que recuperar los datos de la API
+  if (!resultados) {
+    console.log("Solicitando obtención de datos de la API");
+    // obtener datos de api
+    // ----- esta línea imita la recepción de los datos recibidos de la función que los elabora
+    resultados = await obtenerDatos2();
+    // console.log(resultados);
+    // datos API
+    console.log("Almacenando resultados en caché (Local Storage)");
+    localStorage.setItem("resultados", JSON.stringify(resultados));
+    localStorage.setItem("horaResultados", ahora.getTime());
   }
-});
+  console.log("Obtención definitiva de resultados del Backend:");
+  return resultados;
+}
+
+// Obtención definitiva de resultados del Backend:
+resultados = obtenerResultados().then((resultados) => console.log(resultados));
+
+function obtenerResultadosCache() {
+  const cache = new Date(parseInt(localStorage.getItem("horaResultados")));
+  console.log(
+    `Hora de caché de datos: ${cache.getHours()}:${String(
+      cache.getMinutes()
+    ).padStart(2, "0")}`
+  );
+  const min5 = 1000 * 60 * 5;
+
+  console.log(`tiempo transcurrido: ${ahora.getTime() - cache.getTime()}`);
+
+  if (ahora.getTime() - cache.getTime() < min5) {
+    console.log("Recuperando resultados de caché (localStorage)");
+    return JSON.parse(localStorage.getItem("resultados"));
+  } else {
+    // Ya han pasado más de 5min, solicitar datos de la API
+    return false;
+  }
+}
